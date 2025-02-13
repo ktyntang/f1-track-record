8cd6ecfb281533196a50856fa3b846c3c87aa6f9
ktyntang <ktyntang@hotmail.com>
2025-02-14 01:25:19 +0800

    fix: header and background zindex

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,8 +1,8 @@
 const Background = () => {
 	return (
-		<div aria-hidden='true' className='absolute inset-0 grid grid-cols-2 opacity-40'>
-			<div className='from-primary h-56 bg-gradient-to-br to-purple-400 blur-[106px]'></div>
-			<div className='h-32 bg-gradient-to-r from-cyan-400 to-sky-300 blur-[106px]'></div>
+		<div aria-hidden='true' className='fixed inset-0 grid grid-cols-2 opacity-40'>
+			<div className='from-primary h-96 bg-gradient-to-br to-purple-400 blur-[106px]'></div>
+			<div className='h-72 bg-gradient-to-r from-cyan-400 to-sky-300 blur-[106px]'></div>
 		</div>
 	);
 };
