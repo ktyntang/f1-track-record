0c6284be02a2869676ee548fa82ec61d39b9071b
ktyntang <ktyntang@hotmail.com>
2025-02-17 18:29:07 +0800

    chore: add https for dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			src: "/src",
+export default defineConfig(({ mode }) => {
+	const isDevelopment = mode === "development";
+
+	return {
+		plugins: [react(), tailwindcss(), isDevelopment && mkcert()],
+		resolve: {
+			alias: {
+				src: "/src",
+			},
 		},
-	},
+	};
 });
