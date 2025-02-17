5542d08f4becbdb7be390a577a69e378c2ebe900
ktyntang <ktyntang@hotmail.com>
2025-02-17 21:23:18 +0800

    feat: add draft search bar

--- src/components/RacesContainer.tsx
+++ src/components/RacesContainer.tsx
@@ -41,16 +41,27 @@ const RacesContainer: FC = () => {
 		<div className='z-30 mx-auto h-[90dvh] w-full max-w-7xl px-4 md:px-6'>
 			{/* TODO : Handle edge case of min and max f1 years */}
 			{/* TODO : Error display if no races completed yet for current year */}
-			<div className='m-2 flex w-fit flex-row gap-2 rounded-2xl bg-white p-2 shadow-md'>
-				<button className='' onClick={() => setSelectedYear(selectedYear - 1)}>
-					&lt;
-				</button>
+			<div className='flex flex-row justify-start gap-2'>
+				{/* YEAR SELECTION BUTTONS */}
+				<div className='m-2 flex w-fit flex-row gap-2 rounded-2xl bg-white p-2 shadow-md'>
+					<button className='' onClick={() => setSelectedYear(selectedYear - 1)}>
+						&lt;
+					</button>
 
-				<div>{selectedYear}</div>
-				<button onClick={() => setSelectedYear(selectedYear + 1)}>&gt;</button>
+					<div>{selectedYear}</div>
+					<button onClick={() => setSelectedYear(selectedYear + 1)}>&gt;</button>
+				</div>
+
+				{/* TODO: Search bar function */}
+				<div>
+					<input
+						type='text'
+						placeholder='Search for a race..'
+						className='m-2 ml-0 w-48 rounded-2xl bg-white p-2 pl-3 shadow-md'
+					/>
+				</div>
 			</div>
 
-			{/* TODO: Search bar */}
 			<div className='mb-auto flex items-center justify-center'>
 				{isLoading && !hasError && <h3>Loading...</h3>}
 				{hasError && <h1>Something went wrong. Please try again later.</h1>}
