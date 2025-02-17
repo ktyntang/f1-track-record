955794f852de5f5579488a0dd3998789178d8a0f
ktyntang <ktyntang@hotmail.com>
2025-02-17 21:17:54 +0800

    style: brighter bg

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,7 @@
 const Background = () => {
 	return (
-		<div aria-hidden='true' className='fixed inset-0 grid grid-cols-2 opacity-40'>
-			<div className='from-primary h-96 bg-gradient-to-br to-purple-400 blur-[106px]'></div>
+		<div aria-hidden='true' className='fixed inset-0 grid grid-cols-2 opacity-50'>
+			<div className='from-primary h-96 bg-gradient-to-br to-purple-500 blur-[106px]'></div>
 			<div className='h-72 bg-gradient-to-r from-cyan-400 to-sky-300 blur-[106px]'></div>
 		</div>
 	);
